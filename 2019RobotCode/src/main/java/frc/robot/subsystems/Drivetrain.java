a782c3243849260f801968f6bf9e27035c67108d
gatlinf <gatlinfarrington@gmail.com>
2019-01-14 16:05:45 -0700

    added drivetrain subsystem
    i think it is the regular outline with some commented shit idrk

---
+++ 2019RobotCode/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.*;
+
+/**
+ * Add your docs here.
+ */
+
+//  new Talon frontLeft;
+
+public class Drivetrain extends Subsystem {
+  // frontLeft = new Talon(RobotMap.FLTalon);
+  // Talon frontRight = new Talon(FRTalon);
+  // Talon backLeft = new Talon(BLTalon);
+  // Talon backRight = new Talon(BRTalon);
+
+  
+
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+
+    // setDefaultCommand(new drive());
+  }
+}
