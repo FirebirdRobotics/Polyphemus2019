5e58d0e86f6c8c96da69917fcd1974fb1811ca90
Firebird Robotics <43557803+firebirdrobotics-3019@users.noreply.github.com>
2019-01-15 15:48:50 -0700

    Update RobotMap.java

--- 2019RobotCode/bin/main/frc/robot/RobotMap.java
+++ 2019RobotCode/bin/main/frc/robot/RobotMap.java
@@ -14,13 +14,16 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+  // Scale Factors
+  public static double DRIVE_SCALE_FACTOR = 1.0;
+  public static double TURN_SCALE_FACTOR = 1.0;
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
+  // CAN Motor Controller Ports (DriveTrain)
+	public static int leftFront = 2;
+	public static int leftBack = 1;
+	public static int rightFront = 3;
+	public static int rightBack = 4;
+
+  // PWM Ports
+  
 }
