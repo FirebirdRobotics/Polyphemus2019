e5d0dbbee7b2da8fe681b7b9dc6986f23f485100
gatlinf <gatlinfarrington@gmail.com>
2019-02-13 23:05:58 -0700

    encoder counts vars added
    BUT has ports from practice bot

--- 2019RobotCode/bin/main/frc/robot/RobotMap.java
+++ 2019RobotCode/bin/main/frc/robot/RobotMap.java
@@ -15,38 +15,43 @@ package frc.robot;
  */
 public class RobotMap {
   // Scale Factors
-  public static double DRIVE_SCALE_FACTOR = 1.0;
+  public static double DRIVE_SCALE_FACTOR = 0.1;
   public static double TURN_SCALE_FACTOR = 1.0;
 
   // CAN Motor Controller Ports (Drivetrain)
-	public static int leftFront = 2;
-	public static int leftBack = 1;
-	public static int rightFront = 3;
-  public static int rightBack = 4;
+	// public static int leftFront = 0;
+	// public static int leftBack = 1;
+	// public static int rightFront = 2;
+  // public static int rightBack = 3;
+  public static int tempLeftMotor = 1;
+  public static int tempRightMotor = 6;
   
   // also for Drivetrain
   public static boolean orientForward = false;
 
   // PWM Ports
-  public static int elevatorPort = 0; // temporary
-  public static int hatchSystemPort = 1; // temporary
-  public static int frontIn = 0; // temporary
-  public static int frontOut = 1; // temporary
-  public static int backIn = 2;
-  public static int backOut = 3;
-  public static int stiltsPort = 4; // temporary
-	
-  public static int elbowPort = 4; // pivot at the elbow of the arm  TEMP
-  public static int shoulderPort = 5; //shoulder, motor connected directly to elevator box TEMP
+  public static int elevatorPort = 7; // temporary
+  public static int hatchSystemPort = 8; // temporary
+  public static int frontIn = 0; // solenoids
+  public static int frontOut = 1; // solenoids
+  public static int backIn = 2; // solenoids
+  public static int backOut = 3; // solenoids
+  public static int elbowPort = 4; // pivot at the elbow of the arm
+  public static int shoulderPort = 5; //shoulder, motor connected directly to elevator box
 
-  //encoder counts
-  private static int lowCount = 10;
-  private static int midCount = 50;
-  private static int highCount = 100;
+  //encoderCounts
+    //all #s are psuedo until we have the robot
+    //these need to be given values before the placeHatch method can be called because it relies on comparing to these vals
+  public static int lowEle;
+  public static int midEle;
+  public static int hiEle;
 
-  private static int lowPivotCount = 10;
-  private static int midPivotCount = 50;
-  private static int highPivotCount = 100;
+  public static int lowShoulder;
+  public static int midShoulder;
+  public static int hiShoulder;
 
-  private static boolean panelIsUp;
+  public static int lowElbow;
+  public static int midElbow;
+  public static int hiElbow;
+
 }
