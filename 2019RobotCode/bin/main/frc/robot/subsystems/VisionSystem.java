0900d979113f1c13979c2118387fde0f17c27333
Branden Yang <33813750+ricefarmchild@users.noreply.github.com>
2019-02-14 18:02:15 -0700

    Update VisionSystem.java

--- 2019RobotCode/bin/main/frc/robot/subsystems/VisionSystem.java
+++ 2019RobotCode/bin/main/frc/robot/subsystems/VisionSystem.java
@@ -26,7 +26,9 @@ public class VisionSystem extends Subsystem {
   private double targetAngle;
   NetworkTableInstance instance;
   NetworkTable chickenVision;
-  // Timer timer;
+  Timer timer;
+  long yawValue;
+  int timeInt;
 
   public VisionSystem() {
     // Vision
@@ -48,7 +50,7 @@ public class VisionSystem extends Subsystem {
     tapeVision = cargoVision = false;
     driverVision = true;
 
-    // timer = new Timer();
+    timer = new Timer();
   }
 
   @Override
@@ -59,21 +61,67 @@ public class VisionSystem extends Subsystem {
 
   public void visionRoutine(double left, double right) {
     // timer.start();
+    timeInt = 0;
     // System.out.println("vision routine, yaw is: " + tapeYaw.getDouble(0));
-      while (tapeYaw.getDouble(0) >= 2.00 || tapeYaw.getDouble(0) <= -2.00) {
-        
-          if (tapeYaw.getDouble(0) <= -2.00) { // if negative (on right)
+      while (tapeYaw.getDouble(0) > 2.0 || tapeYaw.getDouble(0) < -2.0) {
+        yawValue = (long) Math.abs(tapeYaw.getDouble(0));
+        System.out.println("----");
+        System.out.println("tapeYaw: " + tapeYaw.getDouble(0));
+          if (tapeYaw.getDouble(0) < -2.0) { // if negative (on right)
               // put left backwards & right forwards (rotate counter-clockwise)
               Robot.driveTrain.camDrive(left, -right);
-              System.out.println("negative values");
-          } else if (tapeYaw.getDouble(0) >= 2.00) { // if positive (on right)
+              System.out.println("tapeYaw is on left");
+              System.out.println("timeInt: " + timeInt);
+          }
+          else if (tapeYaw.getDouble(0) > 2.0) { // if positive (on right)
               // put left forwards & right backwards (rotate clockwise)
               Robot.driveTrain.camDrive(-left, right);
-              System.out.println("positive values");
+              System.out.println("tapeYaw is on right");
+              System.out.println("timeInt: " + timeInt);
           }
-          // if (timer.get() > 3.0) {
-          //   break;
-          // }
+          if (timeInt++ > 10) {
+            return;
+          }
+          // use sleeps to control how long robot is running/stopping motors
+          try {
+            Thread.sleep(yawValue * 40); // how long robot runs motors
+            Robot.driveTrain.camDrive(0, 0);
+            Thread.sleep(50); // how long robot sets motors to 0
+
+            System.out.println("stopping motors");
+          } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          }
+      }
+      // Set motors to zero after outside of loop
+      Robot.driveTrain.camDrive(0, 0);
+      System.out.println("final yaw: " + tapeYaw.getDouble(0));
+  }
+
+
+  /*
+
+  Drive TOWARDS THE TAPE STRAIGHT LINE?!
+
+  public double l = .2;
+  public double r = .2; 
+  Timer camTime = new Timer();
+  public void driveToward(){
+    camTime.start();
+    Robot.driveTrain.camDrive(l, r);
+      while(camTime.get() < 3){
+        if(tapeYaw.getDouble(0) <= -1){
+          r += .1;
+          l -= .1;
+          Robot.driveTrain.camDrive(l, r);
+        }else if(tapeYaw.getDouble(0) >= 1){
+          r -= .1;
+          l += .1;
+          Robot.driveTrain.camDrive(l, r);
+        } else Robot.driveTrain.camDrive(.2, .2);
+        
       }
   }
+  */ 
 }
